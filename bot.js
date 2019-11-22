d3824aeb146d3dedaf38b51ba16cb29785a18af3
benluelo <benluelo@hotmail.com>
2019-11-22 00:45:36 -0500

    push update

--- bot.js
+++ bot.js
@@ -2,6 +2,7 @@
 const Discord = require("discord.js")
 const client = new Discord.Client()
 require('dotenv').config({ path: '.env' })
+const chalk = require("chalk")
 
 // #region so many constants!
 
@@ -31,6 +32,13 @@ const bot = "641039236786814976"
 var start = false
 var eventRunning = false
 
+// chalk constants
+const name = chalk.hex("#0000e6")
+const time = chalk.cyanBright.bold
+const rolename = chalk.yellow
+const botOn = chalk.whiteBright.bold.underline
+const date = chalk.keyword('orange')
+
 // #endregion
 
 // new date prototype
@@ -56,8 +64,11 @@ Date.prototype.readableDate = function() {
         case 11: this.month = "December"; break
     }
 
-    // get time
+    // get hour
     this.hour = this.getUTCHours()
+
+    // get minute
+    this.minute = this.getUTCMinutes().toString().padStart(2, "0")
 
     // get day
     switch(this.getUTCDay()){
@@ -74,7 +85,7 @@ Date.prototype.readableDate = function() {
     this.date = this.getUTCDate()
 
     // build readable string
-    return `${this.day}, ${this.month} ${this.date} at ${this.hour}:00`
+    return `${this.day}, ${this.month} ${this.date} at **${this.hour}:${this.minute}**`
 
 }
 
@@ -83,22 +94,22 @@ client.on("ready", () => {
 
     console.log("═".repeat(50) + "\n")
     // log bot running
-    console.log(`${client.user.username} is online!`)
+    console.log(botOn(`${client.user.username} is online!\n`))
 
     // grab today's date
     var d = new Date(new Date().toUTCString())
 
     // #region date debug
-    // console.log("Today: ", d, d.getTime())
-    // console.log("First event: ", eventFirst, eventFirst.getTime())
-    // console.log("ms between first event and now:", ((d.getTime() - eventFirst.getTime())) )
-    // console.log("number of events between first event and now:", ((d.getTime() - eventFirst.getTime()) / hours15 ) )
-    // console.log(((d.getTime() - eventFirst.getTime()) / 3600000))
+    console.log(time("Today- - - - - - - - - - - - - - - - - - - -:"), /*d,*/ d.readableDate(), d.getTime())
+    console.log(time("First event- - - - - - - - - - - - - - - - -:"), eventFirst.readableDate(), eventFirst.getTime())
+    console.log(time("MS between first event and now - - - - - - -:"), ((d.getTime() - eventFirst.getTime())) )
+    console.log(time("number of events between first event and now:"), ((d.getTime() - eventFirst.getTime()) / hours15 ) )
+    console.log(time("Hourss between first event and now - - - - -:"), ((d.getTime() - eventFirst.getTime()) / 3600000))
     // #endregion
 
     // check if event is running
     if(((d.getTime() - eventFirst.getTime()) % hours15) <= (hours1 * 4)){
-        console.log("Event currently running!")
+        console.log(chalk.greenBright.bold.underline("Event currently running!"))
         eventRunning = true
         console.log(((d.getTime() - eventFirst.getTime()) % hours15) / (hours1 * 4))
     }
@@ -108,173 +119,186 @@ client.on("ready", () => {
 
     // console.log("Next event in ms:", nextEventMS)
     let nextEvent = new Date(nextEventMS)
-    console.log("Next event: ", nextEvent)
+    console.log(time("Next event:"), nextEvent.readableDate())
 
     // run event
-    skullalert(nextEventMS)
+    // skullalert(nextEventMS)
 })
 
-/* this is here for future stuffs
+// this is here for future stuffs
 client.on("message", (message) => {
-    if(!start) return
-    //check if message is a dm & ignore it
+
+    //check if message is a dm and if so, ignore it
     if(message.channel.type == "dm") return
-    console.log(message.author.username)
+    console.log(chalk.keyword("lightgreen")("[LOG][MESSAGE] ") + message.author.username + ": " + message.content)
 
-    // start!
-    if(message == "timer start"){
-
-        // set start var to true, so timer doesnt run twice
-        start = true
-
-        // grab today's date
-        var d = new Date()
-
-        // #region date debug
-        // console.log("Today: ", d, d.getTime())
-        // console.log("First event: ", eventFirst, eventFirst.getTime())
-        // console.log("ms between first event and now:", ((d.getTime() - eventFirst.getTime())))
-        // console.log(((d.getTime() - eventFirst.getTime()) % 3600000))
-        // #endregion
-
-        //get time of next event
-        let nextEventMS = d.getTime() + (hours15 - ((d.getTime() - eventFirst.getTime()) % hours15))
-
-        // console.log("Next event in ms:", nextEventMS)
-        let nextEvent = new Date(nextEventMS)
-        console.log("Next event: ", nextEvent.toUTCString())
-
-        // run event
-        skullalert(nextEventMS)
+    // send whennext event is, if requested
+    if(message == "%next event"){
+        message.channel.send(`The next event starts ${new Date(new Date(new Date().toUTCString()).getTime() + (hours15 - ((new Date(new Date().toUTCString()).getTime() - eventFirst.getTime()) % hours15))).readableDate()} UTC!`)
     }
 })
-*/
 
-// skull alert function
-function skullalert(event){
 
-    // console alert
-    console.log("\n\n\033[4m\033[41mSkull Alert!\033[0m\n")
+// // skull alert function
+// function skullalert(event){
 
-    // Get the Guild and store it under the variable "list"
-    const list = client.guilds.get("641023205074796544")
+//     // console alert
+//     console.log("\n\n\033[4m\033[41mSkull Alert!\033[0m\n")
 
-    // loop through roles
-    list.roles.forEach(role => {
+//     // Get the Guild and store it under the variable "list"
+//     const list = client.guilds.get("641023205074796544")
 
-        // skip @everyone and Timer Bot roles
-        if((role.id == everyone) || (role.id == TimerBot)) return
+//     // loop through roles
+//     list.roles.forEach(role => {
 
-        console.log("\033[33mRole name:\033[0m", role.name)
+//         // skip @everyone and Timer Bot roles
+//         if((role.id == everyone) || (role.id == TimerBot)) return
 
-        // loop through each member with said role
-        role.members.forEach(member => {
+//         console.log(rolename("Role name:"), role.name)
 
-            // skip Timer Bot
-            if(member.user.bot) return
+//         // loop through each member with said role
+//         role.members.forEach(member => {
 
-            // switch message per timezone
-            let t = new Array(3)
-            switch(role.id){
-                case timezone_minus_5:
-                    t[0] = new Date((event - (5 * hours1) + (15 * hours1)))
-                    t[1] = new Date((event - (5 * hours1)) + hours4)
-                    t[2] = "GMT -5"
-                    break
-                case timezone_plus_2:
-                    t[0] = new Date((event + (2 * hours1) + (15 * hours1)))
-                    t[1] = new Date((event + (2 * hours1)) + hours4)
-                    t[2] = "GMT +2"
-                    break
-                case timezone_plus_8:
-                    t[0] = new Date((event + (8 * hours1) + (15 * hours1)))
-                    t[1] = new Date((event + (8 * hours1)) + hours4)
-                    t[2] = "GMT +8"
-                    break
-                default:
-                    t[0] = new Date(event  + (15 * hours1))
-                    t[1] = new Date(event + hours4)
-                    t[2] = "GMT"
-            }
+//             // skip Timer Bot
+//             if(member.user.bot) return
 
-            // send actual message
-            console.log("\033[34m"+member.user.tag+"\033[0m", t[2])
-            console.log("Next skull event is at", t[0].readableDate(), "and the time to join the event closes at", t[1].readableDate())
+//             // switch message per timezone
+//             let t = new Array(3)
+//             console.log(new Date(event))
+//             switch(role.id){
+//                 case timezone_minus_5:
+//                     t[0] = new Date((event - (5 * hours1) + (15 * hours1) /* (eventRunning? (15 * hours1): 0) */))
+//                     t[1] = new Date((event - (5 * hours1)) + hours4)
+//                     t[2] = "GMT -5"
+//                     break
+//                 case timezone_plus_2:
+//                     t[0] = new Date((event + (2 * hours1) + (15 * hours1) /* (eventRunning? (15 * hours1): 0) */))
+//                     t[1] = new Date((event + (2 * hours1)) + hours4)
+//                     t[2] = "GMT +2"
+//                     break
+//                 case timezone_plus_8:
+//                     t[0] = new Date((event + (8 * hours1) + (15 * hours1) /* (eventRunning? (15 * hours1): 0) */))
+//                     t[1] = new Date((event + (8 * hours1)) + hours4)
+//                     t[2] = "GMT +8"
+//                     break
+//                 default:
+//                     t[0] = new Date(event  + (15 * hours1))
+//                     t[1] = new Date(event + hours4)
+//                     t[2] = "GMT"
+//             }
+
+//             // send actual message
+//             console.log(name(member.user.tag), t[2])
+//             console.log(
+//                 "Next skull event is at", date(t[0].readableDate()), 
+//                 "\nand the time to join the current event that starts", date(new Date(t[0].getTime() - hours15).readableDate()), 
+//                 "closes at", date(t[1].readableDate()))
             
-            // create embed
-            const embed = new Discord.RichEmbed()
-                .setColor('#000080')
-                .setDescription("💀**Skull Alert!**💀")
-                // .setThumbnail("https://imgur.com/K7pw1Em")
-                .addField('**Closes on:**', t[1].readableDate(), false)
-                .addField('**Next event:**', t[0].readableDate(), false)
-                // .setImage('https://i.imgur.com/wSTFkRM.png')
-                .setTimestamp()
-                .setFooter('benluelo')
+//             // create embed
+//             const embed = new Discord.RichEmbed()
+//                 .setColor('#000080')
+//                 .setDescription("💀**Skull Alert!**💀")
+//                 // .setThumbnail("https://imgur.com/K7pw1Em")
+//                 .addField(`**The event starting** __${new Date(t[0].getTime() - hours15).readableDate()}__ **closes**`, t[1].readableDate(), false)
+//                 .addField('**The next event starts on:**', t[0].readableDate(), false)
+//                 // .setImage('https://i.imgur.com/wSTFkRM.png')
+//                 .setTimestamp()
+//                 .setFooter('benluelo')
             
-            // send embed
-            // member.send(embed)
-        })
-        console.log("")
-    })
+//             // send embed
+//             member.send(embed)
+//         })
+//         console.log("")
+//     })
 
-    // schedule next event
-    setTimeout( () => {
-        skullalert
-    }, (hours15))
+//     // schedule next event
+//     setTimeout( () => {
+//         skullalert
+//     }, (hours15), event)
 
-    // setInterval(skullalert, hours15, (event + hours15))
+//     console.log(time("Next event scheduled for:"), new Date(event + hours15).readableDate(), "UTC")
 
-    // schedule join alert message
-    setTimeout( () => {
-        joinalert
-    }, (hours15 - hours4 - mins10))
-}
+//     // setInterval(skullalert, hours15, (event + hours15))
 
-function joinalert(currentevent, eventrunning){
+//     // schedule join alert message
+//     setTimeout( () => {
+//         joinalert
+//     }, (hours15 - hours4 - mins10))
 
-    // console alert
-    console.log("\n\n\033[4m\033[41mJoin Alert!\033[0m\n")
+//     console.log(time("Join alert scheduled for:"), new Date(event + hours4 - mins10).readableDate(), "UTC")
+// }
 
-    // check if theres an event running
-    if(!eventRunning){
+// function joinalert(){
 
-        // Get the Guild and store it under the variable "list"
-        const list = client.guilds.get("641023205074796544")
+//     // console alert
+//     console.log("\n\n\033[4m\033[41mJoin Alert!\033[0m\n")
 
-        // loop through roles
-        list.roles.forEach(role => {
+//     // Get the Guild and store it in the variable "list"
+//     const list = client.guilds.get("641023205074796544")
 
-            // skip @everyone and Timer Bot roles
-            if((role.id == everyone) || (role.id == TimerBot)) return
+//     // check if theres an event running
+//     if(!eventRunning){
 
-            console.log("\033[33mRole name:\033[0m", role.name)
+//         // IF EVENT NOT RUNNING::
 
-            // loop through each member with said role
-            role.members.forEach(member => {
+//         // loop through roles
+//         list.roles.forEach(role => {
 
-                // skip Timer Bot
-                if(member.user.bot) return
+//             // skip @everyone and Timer Bot roles
+//             if((role.id == everyone) || (role.id == TimerBot)) return
 
-                // switch message per timezone
-                switch(role.id){
-                    case timezone_minus_5:
-                        console.log(member.user.tag, "-5")
-                        break
-                    case timezone_plus_8:
-                        console.log(member.user.tag, "+8")
-                        break
-                    default:
-                        console.log(member.user.tag, "default")
-                }
-            })
-        })
-        
-    }else{
+//             console.log(rolename("Role name:"), role.name)
 
-        //send embed per role saying when the event closes
+//             // loop through each member with said role
+//             role.members.forEach(member => {
 
-    }
-}
+//                 // skip Timer Bot
+//                 if(member.user.bot) return
 
+//                 const embed = new Discord.RichEmbed()
+//                 .setTitle("Alert! Time to join the event closes in 10 minutes!")
+
+//                 // send embed
+//                 member.message.send(embed)
+//             })
+//         })
+
+//     }else{
+
+//         // IF EVENT RUNNING:
+
+//         // loop through roles
+//         list.roles.forEach(role => {
+
+//             // skip @everyone and Timer Bot roles
+//             if((role.id == everyone) || (role.id == TimerBot)) return
+
+//             console.log("\033[33mRole name:\033[0m", role.name)
+
+//             // loop through each member with said role
+//             role.members.forEach(member => {
+
+//                 // skip Timer Bot
+//                 if(member.user.bot) return
+
+//                 // switch message per timezone
+//                 switch(role.id){
+//                     case timezone_minus_5:
+//                         console.log(member.user.tag, "-5")
+//                         break
+//                     case timezone_plus_8:
+//                         console.log(member.user.tag, "+8")
+//                         break
+//                     default:
+//                         console.log(member.user.tag, "default")
+//                 }
+//             })
+//         })
+
+//         //send embed per role saying when the event closes
+
+//     }
+// }
+
+// login
 client.login(process.env.TOKEN)
